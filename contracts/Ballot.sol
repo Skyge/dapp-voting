e1d333afbcf59e4f2eaf08bf2e5ae9ecff5f3c39
skyge <18844062006@163.com>
2019-02-18 17:06:39 +0800

    U Optimize the contract.

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -23,7 +23,7 @@ contract Ballot is Pausable {
 
     // This is a type for a single proposal.
     struct Proposal {
-        uint8 number;       // number of proposal
+        uint8 id;           // number of proposal
         uint32 voteCount;   // number of accumulated votes
     }
 
@@ -33,26 +33,26 @@ contract Ballot is Pausable {
 
     // A dynamically-sized array of `Proposal` structs.
     Proposal[] public proposals;
-    uint8[] public winningProposal_;
 
     // voted event
-    event votedEvent (uint indexed _candidateId);
+    event VotingEvent (uint indexed _candidateId);
 
-    /// @dev Create a new ballot to choose one of `proposalNames`.
+    /// @dev Create a new ballot to choose one of `proposalNumbers`.
     constructor(uint8 proposalNumbers) public {
         // For each of the provided proposal names,
         // create a new proposal object and add it
         // to the end of the array.
         for (uint8 i = 0; i < proposalNumbers; i++) {
             proposals.push(Proposal({
-                number: i,
+                id: i,
                 voteCount: 0
             }));
         }
     }
 
     /// @dev Give your vote 
-    /// to proposal `proposals[proposal].name`.
+    /// to proposal `proposals[proposal].id`. 
+    /// Only valid when contract does not pause.
     function vote(uint8 proposal) public whenNotPaused returns (bool) {
         Voter storage sender = voters[msg.sender];
         require(!sender.hasVoted, "Already voted.");
@@ -65,30 +65,19 @@ contract Ballot is Pausable {
         // changes.
         proposals[proposal].voteCount += 1;
 
-        // trigger voted event
-        emit votedEvent(proposal);
+        // trigger voting event
+        emit VotingEvent(proposal);
 
         return true;
     }
 
-    /// @dev Computes the winning proposal taking all
-    /// previous votes into account.
-    function winningProposal() public whenPaused returns (uint8[] memory) {
-        uint32 winningVoteCount = 0;
-        for (uint8 p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount == winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winningProposal_.push(p);
-            }else if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                for (uint8 i = uint8(winningProposal_.length)-1; i <= 0; i--) {
-                    delete winningProposal_[i];
-                    winningProposal_.length--;
-                }
-                winningProposal_.push(p);
-            }
-        }
+    /// @dev Add a new candidate, but only for the owner.
+    function addCandidate() public onlyOwner returns (bool) {
+        proposals.push(Proposal({
+            id: uint8(proposals.length) - 1,
+            voteCount: 0
+        }));
 
-        return winningProposal_;
+        return true;
     }
 }
