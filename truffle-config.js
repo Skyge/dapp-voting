c682ea1ddf1fde2632463278a6ad1f0c45a6d6c5
skyge <18844062006@163.com>
2019-03-20 14:32:49 +0800

    U Modify deploying file.

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,32 @@
+require('dotenv').config()
+var HDWalletProvider = require("truffle-hdwallet-provider"); 
+var infura_apikey = process.env.INFURA_APIKEY;
+var mnemonic = process.env.MNEMONIC; 
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "localhost",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    kovan: {
+      provider: new HDWalletProvider(mnemonic, "https://kovan.infura.io/"+infura_apikey),
+      network_id: 42,
+      gas: 3012388,
+      gasPrice: 30000000000
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3,
+      gas: 3012388,
+      gasPrice: 30000000000
+    },
+    main: {
+      provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/"+infura_apikey),
+      network_id: 3,
+      gas: 3012388,
+      gasPrice: 1000000000
     }
   }
 };
